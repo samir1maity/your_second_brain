ce1ecea5c7f599cd8140cdd2e5406c05958f1ea4
samir1maity <s879maity@gmail.com>
2025-01-12 02:16:12 +0530

    change mongodb to postgre and wrote user schema

--- backend/src/model/tag.schema.ts
+++
@@ -1,8 +0,0 @@
-import mongoose, { model, Schema, Types } from "mongoose";
-
-const tagSchema = new Schema({
-  title: { type: String, required: true, unique: true },
-});
-
-export const tagModel = model("tag", tagSchema);
-
